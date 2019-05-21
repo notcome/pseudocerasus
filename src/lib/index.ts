678b790a81a808add0b69467567bc75ca072853c
Minsheng Liu <lambda@liu.ms>
2019-05-21 14:56:15 +0800

    Expose ContentEditable in a non-default fashion.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,5 +2,5 @@ import InlineRange from './InlineRange'
 import ContentEditable from './ContentEditable'
 import SelectionHost from './SelectionHost'
 
-export { InlineRange, SelectionHost }
+export { InlineRange, SelectionHost, ContentEditable }
 export default ContentEditable
